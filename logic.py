c72809dc2ca8701b0cc3a6c9299d6d5a84545d3f
JaymeChua <95556211+JaymeChua@users.noreply.github.com>
2025-02-02 18:07:52 +0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -225,39 +225,32 @@ class Biconditional(Sentence):
         return set.union(self.left.symbols(), self.right.symbols())
 
 
-def model_check(knowledge, query):
-    """Checks if knowledge base entails query."""
+def model_check(knowledge, query, model=None):
+    """Checks if knowledge base entails query given an optional model."""
 
     def check_all(knowledge, query, symbols, model):
         """Checks if knowledge base entails query, given a particular model."""
-
-        # If model has an assignment for each symbol
         if not symbols:
-
-            # If knowledge base is true in model, then query must also be true
             if knowledge.evaluate(model):
-                return query.evaluate(model)
-            return True
-        else:
+                if query.evaluate(model) == True:
+                    print(query)
 
-            # Choose one of the remaining unused symbols
+        else:
             remaining = symbols.copy()
             p = remaining.pop()
 
-            # Create a model where the symbol is true
             model_true = model.copy()
             model_true[p] = True
 
-            # Create a model where the symbol is false
             model_false = model.copy()
             model_false[p] = False
 
-            # Ensure entailment holds in both models
-            return (check_all(knowledge, query, remaining, model_true) and
-                    check_all(knowledge, query, remaining, model_false))
+            return check_all(knowledge, query, remaining, model_true) and check_all(knowledge, query, remaining, model_false)
 
-    # Get all symbols in both knowledge and query
-    symbols = set.union(knowledge.symbols(), query.symbols())
+    symbols = knowledge.symbols().union(query.symbols())
 
-    # Check that knowledge entails query
-    return check_all(knowledge, query, symbols, dict())
+    if model is None:
+        model = {}
+
+    return check_all(knowledge, query, symbols, model)
+
